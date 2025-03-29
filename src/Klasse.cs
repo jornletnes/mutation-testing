46f7a08b5bb11ec76b2a2fe796fd022a4f95d85f
walwiljorn <jorn.letnes@walwil.com>
2025-03-29 13:32:43 +0100

    first pass

---
+++ src/Klasse.cs
@@ -0,0 +1,13 @@
+namespace mutation_testing;
+
+public class Klasse
+{
+    public int Funksjon1()
+    {
+        var inkrement = 1;
+        var tallA = 1;
+        var tallB = tallA + inkrement;
+        return tallB;
+    }
+
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace mutation_testing.Tests;
+
+public class Tests
+{
+
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        var underTest = new Klasse();
+        
+        // Act
+        var actual = underTest.Funksjon1();
+        
+        // Assert
+        Assert.That(actual, Is.EqualTo(2));
+    }
+}
